04aaa5f3132f69f8e3b7eedefa1d8c93084f4d7d
zhouhailin <zhouhailin555@aliyun.com>
2021-01-30 21:20:03 +0800

    新增: EslEventUtil/VariableUtil工具类

---
+++ freeswitch-esl/src/main/java/link/thingscloud/freeswitch/esl/constant/VariableConstant.java
@@ -0,0 +1,13 @@
+package link.thingscloud.freeswitch.esl.constant;
+
+/**
+ * @author zhouhailin
+ * @version 1.5.0
+ */
+public class VariableConstant {
+
+    public static final String PREFIX = "variable_";
+
+    private VariableConstant() {
+    }
+}
---
+++ freeswitch-esl/src/main/java/link/thingscloud/freeswitch/esl/util/VariableUtil.java
@@ -0,0 +1,11 @@
+package link.thingscloud.freeswitch.esl.util;
+
+/**
+ * @author zhouhailin
+ * @version 1.5.0
+ */
+public class VariableUtil {
+
+    private VariableUtil() {
+    }
+}
