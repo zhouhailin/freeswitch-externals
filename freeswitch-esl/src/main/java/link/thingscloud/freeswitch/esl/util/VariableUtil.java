7e9f985929ceadb917b17b1606cb6de9a430bb8c
zhouhailin <zhouhailin555@aliyun.com>
2021-03-20 16:56:51 +0800

    新增: VariableUtil类

--- freeswitch-esl/src/main/java/link/thingscloud/freeswitch/esl/util/VariableUtil.java
+++ freeswitch-esl/src/main/java/link/thingscloud/freeswitch/esl/util/VariableUtil.java
@@ -1,11 +1,27 @@
 package link.thingscloud.freeswitch.esl.util;
 
+import link.thingscloud.freeswitch.esl.transport.event.EslEvent;
+
 /**
  * @author zhouhailin
- * @version 1.5.0
+ * @version 1.5.1
  */
 public class VariableUtil {
 
+    private static final String VARIABLE_PREFIX = "variable_";
+
+    public static String get(EslEvent event, String key) {
+        return event.getEventHeaders().get(key);
+    }
+
+    public static String getVar(EslEvent event, String key) {
+        if (key.startsWith(VARIABLE_PREFIX)) {
+            return event.getEventHeaders().get(key);
+        }
+        return event.getEventHeaders().get(VARIABLE_PREFIX + key);
+    }
+
     private VariableUtil() {
     }
+
 }
