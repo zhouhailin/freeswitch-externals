e8d1a9f7c01a45e63d589b7b5a21f01efc724a90
zhouhailin <zhouhailin555@aliyun.com>
2021-04-07 17:23:33 +0800

    迭代: 1.5.2.RELEASE

--- freeswitch-esl/src/main/java/link/thingscloud/freeswitch/esl/util/VariableUtil.java
+++ freeswitch-esl/src/main/java/link/thingscloud/freeswitch/esl/util/VariableUtil.java
@@ -2,6 +2,8 @@ package link.thingscloud.freeswitch.esl.util;
 
 import link.thingscloud.freeswitch.esl.transport.event.EslEvent;
 
+import java.util.Map;
+
 /**
  * @author zhouhailin
  * @version 1.5.1
@@ -14,11 +16,23 @@ public class VariableUtil {
         return event.getEventHeaders().get(key);
     }
 
+    public static long getLongVar(EslEvent event, String key) {
+        return Long.parseLong(getVar(event.getEventHeaders(), key));
+    }
+
+    public static int getIntVar(EslEvent event, String key) {
+        return Integer.parseInt(getVar(event.getEventHeaders(), key));
+    }
+
     public static String getVar(EslEvent event, String key) {
+        return getVar(event.getEventHeaders(), key);
+    }
+
+    public static String getVar(Map<String, String> eventHeaders, String key) {
         if (key.startsWith(VARIABLE_PREFIX)) {
-            return event.getEventHeaders().get(key);
+            return eventHeaders.get(key);
         }
-        return event.getEventHeaders().get(VARIABLE_PREFIX + key);
+        return eventHeaders.get(VARIABLE_PREFIX + key);
     }
 
     private VariableUtil() {
