2fbe7afd771cefc73c10f1ccd8af4491f678fb38
zhouhailin <zhouhailin555@aliyun.com>
2023-10-20 10:14:39 +0800

    [Update]: merge rename project commit and resolve conflict

--- freeswitch-esl-spring-boot-starter-example/src/main/java/link/thingscloud/freeswitch/esl/spring/boot/starter/example/ExampleInboundClient.java
+++ freeswitch-esl-spring-boot-starter-example/src/main/java/link/thingscloud/freeswitch/esl/spring/boot/starter/example/ExampleInboundClient.java
@@ -22,8 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 /**
  * @author zhouhailin
  */
