d32265fb8c23865ad03eb27c6b7eaaba74081f91
hani-code-explorer <hani-code-explorer@aliyun.com>
2025-01-08 18:03:28 +0800

    feat(freeswitch-esl-example): 添加 EslInboundClientExample2 示例类
    - 新增 EslInboundClientExample2 类，演示如何使用 InboundClient 连接到 FreeSWITCH 服务器
    - 实现了事件监听和服务器连接状态监听功能
    - 添加了对服务器地址选项的示例操作

---
+++ freeswitch-esl-example/src/main/java/link/thingscloud/freeswitch/esl/example/EslInboundClientExample2.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package link.thingscloud.freeswitch.esl.example;
+
+import link.thingscloud.freeswitch.esl.IEslEventListener;
+import link.thingscloud.freeswitch.esl.InboundClient;
+import link.thingscloud.freeswitch.esl.ServerConnectionListener;
+import link.thingscloud.freeswitch.esl.inbound.option.InboundClientOption;
+import link.thingscloud.freeswitch.esl.inbound.option.ServerOption;
+import link.thingscloud.freeswitch.esl.transport.event.EslEvent;
+
+/**
+ * <p>EslInboundClientExample class.</p>
+ *
+ * @author : <a href="mailto:ant.zhou@aliyun.com">zhouhailin</a>
+ * @version 1.0.0
+ */
+public class EslInboundClientExample2 {
+
+    /**
+     * <p>main.</p>
+     *
+     * @param args an array of {@link String} objects.
+     */
+    public static void main(String[] args) {
+        InboundClientOption option = new InboundClientOption();
+
+        option.defaultPassword("ClueCon")
+                .addServerOption(new ServerOption("172.16.44.246", 8021));
+        option.addEvents("all");
+
+        option.addListener(new IEslEventListener() {
+            @Override
+            public void eventReceived(String addr, EslEvent event) {
+                System.out.println(addr);
+                System.out.println(event);
+            }
+
+            @Override
+            public void backgroundJobResultReceived(String addr, EslEvent event) {
+                System.out.println(addr);
+                System.out.println(event);
+            }
+        });
+
+        option.serverConnectionListener(new ServerConnectionListener() {
+            @Override
+            public void onOpened(ServerOption serverOption) {
+                System.out.println("---onOpened--");
+            }
+
+            @Override
+            public void onClosed(ServerOption serverOption) {
+                System.out.println("---onClosed--");
+            }
+        });
+
+        InboundClient inboundClient = InboundClient.newInstance(option);
+
+        inboundClient.start();
+
+
+        System.out.println(option.serverAddrOption().first());
+        System.out.println(option.serverAddrOption().last());
+        System.out.println(option.serverAddrOption().random());
+
+
+    }
+
+}
