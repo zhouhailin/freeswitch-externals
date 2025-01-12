026b7867b52e41ac07ccce978e4d818cb599fc16
hani-code-explorer <hani-code-explorer@aliyun.com>
2025-01-12 12:03:57 +0800

    feat(freeswitch-esl): 新增心跳事件处理器
    - 添加了 NewHeartbeatEslEventHandler 类来处理心跳事件
    - 使用 @EslEventName 注解指定处理的事件类型为 HEARTBEAT- 实现了 EslEventHandler接口的 handle 方法来记录心跳事件信息

---
+++ freeswitch-esl-spring-boot-starter-example/src/main/java/link/thingscloud/freeswitch/esl/spring/boot/starter/example/NewHeartbeatEslEventHandler.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package link.thingscloud.freeswitch.esl.spring.boot.starter.example;
+
+import link.thingscloud.freeswitch.esl.constant.EventNames;
+import link.thingscloud.freeswitch.esl.spring.boot.starter.annotation.EslEventName;
+import link.thingscloud.freeswitch.esl.spring.boot.starter.handler.EslEventHandler;
+import link.thingscloud.freeswitch.esl.transport.event.EslEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>HeartbeatEslEventHandler class.</p>
+ *
+ * @author : <a href="mailto:ant.zhou@aliyun.com">zhouhailin</a>
+ * @version 1.0.0
+ */
+@Slf4j
+@Component
+@EslEventName(EventNames.HEARTBEAT)
+public class NewHeartbeatEslEventHandler implements EslEventHandler {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handle(String addr, EslEvent event) {
+        log.info("HeartbeatEslEventHandler handle addr[{}] EslEvent[{}].", addr, event);
+    }
+}
