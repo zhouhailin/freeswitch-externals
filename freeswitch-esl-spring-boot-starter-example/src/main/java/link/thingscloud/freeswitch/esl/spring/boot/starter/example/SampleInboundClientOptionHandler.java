a9057e4d82046e7503bb95d9db262b6648365415
hani-code-explorer <hani-code-explorer@aliyun.com>
2025-02-08 08:48:50 +0800

    feat(esl): 添加示例入站客户端选项处理器
    - 新增 SampleInboundClientOptionHandler 类，实现 AbstractInboundClientOptionHandler- 重写 intercept 方法，用于

---
+++ freeswitch-esl-spring-boot-starter-example/src/main/java/link/thingscloud/freeswitch/esl/spring/boot/starter/example/SampleInboundClientOptionHandler.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package link.thingscloud.freeswitch.esl.spring.boot.starter.example;
+
+import link.thingscloud.freeswitch.esl.inbound.option.InboundClientOption;
+import link.thingscloud.freeswitch.esl.inbound.option.ServerOption;
+import link.thingscloud.freeswitch.esl.spring.boot.starter.handler.AbstractInboundClientOptionHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * <p>ExampleInboundClientOptionHandler class.</p>
+ *
+ * @author zhouhailin
+ * @version 1.0.0
+ */
+@Slf4j
+@Component
+public class SampleInboundClientOptionHandler extends AbstractInboundClientOptionHandler {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void intercept(InboundClientOption inboundClientOption) {
+        List<ServerOption> serverOptions = inboundClientOption.serverOptions();
+        log.info("serverOptions before : {}", serverOptions);
+        log.info("serverOptions before : {}", serverOptions);
+//        serverOptions.clear();
+//        serverOptions.add(new ServerOption("127.0.0.8", 8021));
+//        log.info("serverOptions after  : {}", serverOptions);
+    }
+}
